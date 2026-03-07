dc48cb0bf2c35be47b08b970bc051bcb4c92f597
ankit-creolestudios <ankit.kumar@creolestudios.com>
2026-03-07 14:09:45 +0530

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
    <div>
-      <h3>TaskFlow Pro</h3>
+      <h3 className='text-amber-500'>TaskFlow Pro</h3>
     </div>
   )
 }
