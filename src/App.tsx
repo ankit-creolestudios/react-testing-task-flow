7cfe9802b9a733f300feee28fd9f41f7672b7671
ankit-creolestudios <ankit.kumar@creolestudios.com>
2026-03-07 14:17:30 +0530

    feat: setup tailwindcss and shadcn ui system

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
    <div>
       <h3 className='text-amber-500'>TaskFlow Pro</h3>
+       <div className="flex h-screen items-center justify-center">
+      <Button>Click me</Button>
+    </div>
     </div>
   )
 }
